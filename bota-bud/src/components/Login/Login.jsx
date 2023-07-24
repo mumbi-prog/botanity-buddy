0b9b087fb3f3752fa3b3791fbc6aed7adef24565
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 16:08:06 +0300

    Made POST request with username and password

---
+++ bota-bud/src/components/Login/Login.jsx
@@ -0,0 +1,26 @@
+// components/Login.js
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:9292/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
+  return 
+};
+
+export default Login;
