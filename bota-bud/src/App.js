94a552487be553d563119e23821a0b63918ce1e5
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 14:44:45 +0300

    Display Landing Page with Login Button

--- bota-bud/src/App.js
+++ bota-bud/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
-  return;
+  return (
+    <div>
+      <LandingPage/>
+    </div>
+  )
 }
 
 export default App;
