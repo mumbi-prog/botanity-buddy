9a5adfff6047e9b3076f440052ca288cf32e5a75
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 16:33:20 +0300

    Show dashboard upon successful login

--- bota-bud/src/App.js
+++ bota-bud/src/App.js
@@ -4,13 +4,19 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+    const handleLoginSuccess = () => {
+      setIsLoggedIn(true);
+    };
+
+    return (
+      <div className="App">
+        <LandingPage />
+        
+        {isLoggedIn ? <Dashboard /> : <Login onLoginSuccess={handleLoginSuccess} />}
+      </div>
+    );
 }
 
 export default App;
