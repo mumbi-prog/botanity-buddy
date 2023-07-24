92a26f268076743353dac90707059077e6f2e20a
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 18:24:29 +0300

    Add handleLoginSuccess function to use isLoggedIn state on successful login

--- bota-bud/src/components/Login/Login.jsx
+++ bota-bud/src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function Login() {
+function Login({ onLoginSuccess }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -17,6 +17,7 @@ function Login() {
             // handle successful login by user
             if (data.success) {
             console.log('Login successful!');
+            onLoginSuccess();
             } else {
                 // handle unsuccwssful login by user
             console.error('Login failed. Please check your credentials.');
