1ce7ffd76d54bab12f2a38204a54ce36407a1ccd
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 16:25:11 +0300

    Made minor changes

--- bota-bud/src/components/Login/Login.jsx
+++ bota-bud/src/components/Login/Login.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  function handleLogin() {
-    fetch('http://localhost:9292/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // handle successful login by user
-        if (data.success) {
-          console.log('Login successful!');
-        } else {
-            // handle unsuccwssful login by user
-          console.error('Login failed. Please check your credentials.');
-        }
-      })
-      .catch((error) => {
-        console.error('Error during login:', error);
-      });
-  }
+    function handleLogin() {
+        fetch('http://localhost:9292/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            // handle successful login by user
+            if (data.success) {
+            console.log('Login successful!');
+            } else {
+                // handle unsuccwssful login by user
+            console.error('Login failed. Please check your credentials.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error during login:', error);
+        });
+    }
 
 //   login form layout
 
@@ -33,21 +33,21 @@ function Login() {
     <div>
       <h2>Login</h2>
       <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+            Username:
+            <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
       </label>
       <br />
       <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+            Password:
+            <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
       </label>
       <br />
       <button onClick={handleLogin}>Login</button>
