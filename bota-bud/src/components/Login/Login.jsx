ba186a0f773ebabddc7f409eb569589e0588353f
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 16:20:19 +0300

    Added login form layout and input fields

--- bota-bud/src/components/Login/Login.jsx
+++ bota-bud/src/components/Login/Login.jsx
@@ -1,32 +1,57 @@
 // components/Login.js
 import React, { useState } from 'react';
 
-const Login = () => {
+function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://localhost:9292/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
+  function handleLogin() {
+    fetch('http://localhost:9292/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log('Login successful!');
+        } else {
+          console.error('Login failed. Please check your credentials.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error during login:', error);
       });
-      const data = await response.json();
+  }
 
-      if (response.ok) {
-        console.log('Login successful!');
-      } else {
-        console.error('Login failed. Please check your credentials.');
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-    }
-  };
+//   login form layout
 
-  return 
-};
+  return (
+    <div>
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
 
 export default Login;
