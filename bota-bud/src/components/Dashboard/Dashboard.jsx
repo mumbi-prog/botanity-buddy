e4616df1354e74b31168aea694dfcb5d8256674d
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-25 05:40:15 +0300

    Imported plantcard and details to the dashboard

--- bota-bud/src/components/Dashboard/Dashboard.jsx
+++ bota-bud/src/components/Dashboard/Dashboard.jsx
@@ -1,35 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import PlantCard from '../PlantCard/PlantCard';
+import PlantDetails from '../PlantDetails/PlantDetails';
+import '../PlantCard/PlantCard.css'
 
 function Dashboard() {
-    const [plants, setPlants] = useState([]);
+  const [plants, setPlants] = useState([]);
+  const [selectedPlant, setSelectedPlant] = useState(null);
 
-    useEffect(() => {
-        fetchPlants();
-    }, []);
+  useEffect(() => {
+    fetchPlants();
+  }, []);
 
-    function fetchPlants() {
-        fetch('http://localhost:9292/plants')
-        .then((response) => response.json())
-        .then((data) => setPlants(data))
-        .catch((error) => console.error('Error fetching plants:', error));
-    }
+  function fetchPlants() {
+    fetch('http://localhost:9292/plants')
+      .then((response) => response.json())
+      .then((data) => setPlants(data))
+      .catch((error) => console.error('Error fetching plants:', error));
+  }
 
-    function renderPlants() {
-        return plants.map((plant) => (
-        <div key={plant.id}>
-            <h4>{plant.name}</h4>
-            <p>Species: {plant.species}</p>
-            <img src={plant.image_url} alt={plant.name} />
-        </div>
-        ));
-    }
+  const handlePlantCardClick = (plantId) => {
+    const selectedPlant = plants.find((plant) => plant.id === plantId);
+    setSelectedPlant(selectedPlant);
+  };
 
-    return (
-        <div>
-        <h2>Plant Collection</h2>
-        {renderPlants()}
-        </div>
-    );
+  return (
+    <div>
+      <h2>Plant Collection</h2>
+      <div className="plant-card-container">
+        {plants.map((plant) => (
+          <PlantCard key={plant.id} plant={plant} onClick={handlePlantCardClick} />
+        ))}
+      </div>
+      {selectedPlant && <PlantDetails plant={selectedPlant} />}
+    </div>
+  );
 }
 
 export default Dashboard;
