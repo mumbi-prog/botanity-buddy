5d0f5d2e946e8128d0c316875ec904a01fe5402b
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-26 14:49:00 +0300

    Added state to control login form visibility

--- bota-bud/src/components/LandingPage/LandingPage.jsx
+++ bota-bud/src/components/LandingPage/LandingPage.jsx
@@ -2,28 +2,31 @@ import React from 'react'
 import './LandingPage.css'
 import logoImage from '../../images/logo02.png'
 import bgImg from '../../images/bg01.png'
+import Login from '../Login/Login'
 
 function LandingPage() {
-  return (
-    <div>
-      <nav className='navbar' id='navbar'>
-        <div className="logo">
-            <img src={logoImage} alt="Botanical Buddy" className="logo-image" />
-            <span className="logo-text">Botanical Buddy</span>
+    const [showLoginForm, setShowLoginForm] = useState(false);
+
+    return (
+        <div>
+        <nav className='navbar' id='navbar'>
+            <div className="logo">
+                <img src={logoImage} alt="Botanical Buddy" className="logo-image" />
+                <span className="logo-text">Botanical Buddy</span>
+            </div>
+            <button className='login-button'>Login</button>
+        </nav>
+        <div className='home-content'>
+            <div className="page-text">
+                <h3>Botanical Buddy: <br />Your Green Paradise Awaits</h3>
+                <p>Cultivate Your Green Oasis with Botanical Buddy! Discover the Joy of Growing and Caring for Your Plants, Let Nature Thrive at Your Fingertips!</p>
+            </div>
+            <div className="background-image">
+                <img src={bgImg} alt="Botanical Buddy" className="bkgimg" />
+            </div>
         </div>
-        <button className='login-button'>Login</button>
-      </nav>
-      <div className='home-content'>
-        <div className="page-text">
-            <h3>Botanical Buddy: <br />Your Green Paradise Awaits</h3>
-            <p>Cultivate Your Green Oasis with Botanical Buddy! Discover the Joy of Growing and Caring for Your Plants, Let Nature Thrive at Your Fingertips!</p>
         </div>
-        <div className="background-image">
-            <img src={bgImg} alt="Botanical Buddy" className="bkgimg" />
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default LandingPage
