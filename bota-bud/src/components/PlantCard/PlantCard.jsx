6f0efeba2bf81c62b50da08bad07244d8d285008
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-26 18:58:12 +0300

    Added edit and delete icons

--- bota-bud/src/components/PlantCard/PlantCard.jsx
+++ bota-bud/src/components/PlantCard/PlantCard.jsx
@@ -6,8 +6,14 @@ function PlantCard({ plant, onClick }) {
     <div className="plant-card" onClick={() => onClick(plant.id)}>
       <img src={plant.image_url} alt={plant.name} />
       <div className="plant-info">
-        <h3>{plant.name}</h3>
-        <p>Species: {plant.species}</p>
+        <div className='e-and-d'>
+            <i class='bx bx-edit-alt'></i>
+            <i class='bx bx-trash'></i>
+        </div>
+        <div className="info-holder">
+            <h3>{plant.name}</h3>
+            <p>Species: {plant.species}</p>
+        </div>
       </div>
     </div>
   );
