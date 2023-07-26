33908de54c39e3df9636808c27b91b60d52dbfba
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-27 01:01:39 +0300

    Added handleDeleteClick function to confirm plant deletion

--- bota-bud/src/components/PlantCard/PlantCard.jsx
+++ bota-bud/src/components/PlantCard/PlantCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './PlantCard.css';
 
-function PlantCard({ plant, onClick, isSelected }) {
+function PlantCard({ plant, onClick, isSelected, onDelete }) {
+
+  const handleDeleteClick = (e) => {
+    e.stopPropagation();
+    const confirmDelete = window.confirm('Sure about deleting the plant??');
+    if (confirmDelete) {
+        onDelete(plant.id);
+    }
+  };
+
   return (
     <div className={`plant-card ${isSelected ? 'selected' : ''}`}>
       <img src={plant.image_url} alt={plant.name} />
@@ -9,7 +18,7 @@ function PlantCard({ plant, onClick, isSelected }) {
         <div className='e-and-d'>
             <i className="bx bx-expand" title="expand" onClick={() => onClick(plant.id)}></i>
             <i class='bx bx-edit-alt'></i>
-            <i class='bx bx-trash'></i>
+            <i class='bx bx-trash' onClick={handleDeleteClick}></i>
         </div>
         <div className="info-holder">
             <h3>{plant.name}</h3>
