25bd622cb6a45780e915a21af0343299833c2d7e
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-27 06:49:38 +0300

    Implemented the Add care task

--- bota-bud/src/components/PlantDetails/PlantDetails.jsx
+++ bota-bud/src/components/PlantDetails/PlantDetails.jsx
@@ -15,8 +15,43 @@ function PlantDetails({ plant }) {
         );
     };
 
+    const [newTask, setNewTask] = useState({
+      name: '',
+      description: '',
+      due_date: '',
+    });
+
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setNewTask((prevTask) => ({ ...prevTask, [name]: value }));
+    };
+
     const toggleAddTaskForm = () => {
       setShowAddTaskForm(!showAddTaskForm);
+    };
+
+    const handleAddTask = () => {
+
+      fetch('http://localhost:9292/care_tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: newTask.name,
+          description: newTask.description,
+          due_date: newTask.due_date,
+          plant_id: plant.id,
+        }),
+      })
+        .then((response) => response.json())
+        .then((createdTask) => {
+          setCareTasks((prevTasks) => [...prevTasks, createdTask]);
+          toggleAddTaskForm();
+        })
+        .catch((error) => {
+          console.error('Error adding care task:', error);
+        });
     };
 
     const handleBackToDashboard = () => {
@@ -67,6 +102,42 @@ function PlantDetails({ plant }) {
 
         {showAddTaskForm && (
         <div className="add-task-form">
+          <h3>Add New Care Task</h3>
+          <form>
+            <div className="form-group">
+              <label>Name:</label>
+              <input
+                type="text"
+                name="name"
+                value={newTask.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Description:</label>
+              <input
+                type="text"
+                name="description"
+                value={newTask.description}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Due Date:</label>
+              <input
+                type="date"
+                name="due_date"
+                value={newTask.due_date}
+                onChange={handleInputChange}
+              />
+              </div>
+            <button type="button" onClick={handleAddTask}>
+              Save
+            </button>
+            <button type="button" onClick={toggleAddTaskForm}>
+              Cancel
+            </button>
+          </form>
         </div>
       )}
         </div>
