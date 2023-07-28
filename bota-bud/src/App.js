989c1e1f8d3bc6e6360a644dad10a51e0d7f25b6
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-28 17:29:19 +0300

    Final changes

--- bota-bud/src/App.js
+++ bota-bud/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import Login from './components/Login/Login';
@@ -8,13 +8,22 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
 
+  useEffect(() => {
+    const isLoggedInLocalStorage = localStorage.getItem('isLoggedIn');
+    if (isLoggedInLocalStorage) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
     setShowLogin(false);
+    localStorage.setItem('isLoggedIn', true);
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
