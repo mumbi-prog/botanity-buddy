58c1aad6ce6ca507a911333e26164aa73ace69a4
mumbi <mumbi099@gmail.com>
2024-05-24 15:11:52 +0300

    update on botabud landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './LandingPage.css';
-import logoImage from '../../images/logo02.png';
-import bgImg from '../../images/bg01.png';
+import lpimg from '../../images/lpimg1.png';
 
 function LandingPage({ onLoginButtonClick }) {
   return (
@@ -9,25 +8,19 @@ function LandingPage({ onLoginButtonClick }) {
 
       <div className='content-wrapper'>
           <div className='navbar' id='navbar'>
-            <div className="logo">
-              <img src={logoImage} alt="Botanical Buddy" className="logo-image" />
-              <span className="logo-text">Botanical Buddy</span>
-            </div>
+            <h3 className="logo-text">Botanical Buddy</h3>
             {/* Call onLoginButtonClick function when the button is clicked */}
-            <button className='login-button' onClick={onLoginButtonClick}>
-              Login
-            </button>
+            <button className='login-button' onClick={onLoginButtonClick}> Login </button>
           </div>
 
           <div className='home-content'>
               <div className="page-text">
                 <h3>Botanical Buddy: <br />Your Green Paradise Awaits</h3>
                 <p>Cultivate Your Green Oasis with Botanical Buddy! Discover the Joy of Growing and Caring for Your Plants, Let Nature Thrive at Your Fingertips!</p>
-              </div>
-              <div className="background-image">
-                <img src={bgImg} alt="Botanical Buddy" className="bkgimg" />
+                <button className='login-btn2'>Explore your plant list</button>
               </div>
           </div>
+          <img src={lpimg} alt="" className='lpimg'/>
 
       </div>
     </div>
